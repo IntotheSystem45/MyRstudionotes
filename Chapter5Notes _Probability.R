a15ee17430b2c0d1d48f0cd19c9c2bb8f2cbc2ae
IntotheSystem45 <100016186+IntotheSystem45@users.noreply.github.com>
2023-04-24 07:43:13 -0700

    Update Chapter5Notes _Probability.R

--- Chapter5Notes _Probability.R
+++ Chapter5Notes _Probability.R
@@ -1,3 +1,43 @@
 Give events A and B
-Mutually exclusive - No overlap between events A and B
-Not mutuallay exclusive - There is overlap, the probalbity of both happening isn't 0
+Mutually exclusive - No overlap between events A and B e.g. 
+Not mutuallay exclusive - There is overlap, the probalbity of both happening isn't 0 
+
+Sample size e.g: S = (1, 2, 3, 4, 5, 6)
+
+
+Proablaites  can only be between 0-1.
+0 - 0.2 - veery unlikely
+0.2 - 0.4 - unlikley 
+0.4 - 0.5 - less likely to happen than not
+0.5 - 0.6 - more likely happen than not
+0.6 - 0.8 - likely 
+0.8 - 1 - Very likely
+
+- Compound events - 
+Or - General addition rule
+P(A or B) = P(A) + P(B) - P(A and B)
+
+
+Conugnecy tables:
+Find the totals of all rows and columns 
+Use the addition rule
+
+Given - P(X|G) = X/G - the given value is the denominator 
+
+Compliments -A^c - the complment of A
+P(A^c) = 1 - P(A)
+Examples: P(x < 4), then the comploment is P (>= 4), 
+
+And - General mutliplacaiton rule 
+P(A and B) = P(A)P(A|B)
+
+Independant - If the occurnece of 1 event doenst effect the other 
+P(A)P(B) - use this for indepedsnt events
+
+Depedant - 
+example - P(A) = 3/10 P(B) = 2/9, then P(A and B) = 3/10* 2/9 = 
+
+     - Distreete disrrbutions - 
+   
+   
+Expected value E(x) = Mean: M1(W1) + M2(W2) + ... + MN(WN) # weifgted mean
