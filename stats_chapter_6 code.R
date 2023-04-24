c5030884234cfe0144e37c5414ef24d06a2ded03
IntotheSystem45 <100016186+IntotheSystem45@users.noreply.github.com>
2023-04-24 07:49:31 -0700

    Create stats_chapter_6 code.R

---
+++ stats_chapter_6 code.R
@@ -0,0 +1,13 @@
+6.2 
+Binomial distrbution - there are two possiblies 
+
+x = #
+n = # data size
+p = #probablity 
+dbinom(x, n, p,) 
+
+sum(pbinom(x:a,n p)) # x:a is the range of the values 
+
+P(x > 6) 
+
+
